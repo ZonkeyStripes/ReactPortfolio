90b083501da5a2c16bec3458d53c86505876c6bf
Harley <Zonkeystripes@hotmail.com>
2020-06-02 14:04:12 -0700

    Updates

---
+++ precache-manifest.dc0e70ceab652fd6e6ef22c0c3eade44.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bfb85bf677f88655aa3b1ef4fb8486",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "70d92da19cc0e605bb63",
+    "url": "/ReactPortfolio/static/css/2.f2c7665a.chunk.css"
+  },
+  {
+    "revision": "adb2dcce3e801467c832",
+    "url": "/ReactPortfolio/static/css/main.158f2402.chunk.css"
+  },
+  {
+    "revision": "70d92da19cc0e605bb63",
+    "url": "/ReactPortfolio/static/js/2.3dbe034e.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/ReactPortfolio/static/js/2.3dbe034e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb2dcce3e801467c832",
+    "url": "/ReactPortfolio/static/js/main.9d3da4fb.chunk.js"
+  },
+  {
+    "revision": "4ae37d521d2bb28cdbdb",
+    "url": "/ReactPortfolio/static/js/runtime-main.17f5a5c9.js"
+  },
+  {
+    "revision": "51dc20e4a5e177f1beb7712b763b4226",
+    "url": "/ReactPortfolio/static/media/background.51dc20e4.jpg"
+  },
+  {
+    "revision": "4f6c35788c04bf6e8ea7c04c8920b124",
+    "url": "/ReactPortfolio/static/media/covidshot.4f6c3578.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPortfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d5e231944209e2ab09c0dd23e205ce87",
+    "url": "/ReactPortfolio/static/media/passgenShot.d5e23194.png"
+  },
+  {
+    "revision": "addf112c1963015a1edfbcb84c324d43",
+    "url": "/ReactPortfolio/static/media/profilepic.addf112c.jpg"
+  },
+  {
+    "revision": "2f45ad29128188e4a694c1ddb7fe1c4d",
+    "url": "/ReactPortfolio/static/media/recipefindShot.2f45ad29.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ReactPortfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ReactPortfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ReactPortfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ReactPortfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
